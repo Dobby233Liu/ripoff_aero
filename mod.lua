d29774779cd3437752191fa1e89e7b4a6ff90acc
Liu Wenyuan <15816141883@163.com>
2024-02-19 07:17:28 +0800

    bg: funky mode 3

--- mod.lua
+++ mod.lua
@@ -11,24 +11,33 @@ Mod.WAVY_LAKE = love.graphics.newShader[[
     extern number mag = 2.0;
     extern vec2 thickness = vec2(1.0, 0.0);
 
-    extern number clamp_chunk_dim = 1.0; // -1 = crop out, 0 = no clamping
+    extern number clamp_chunk_dim = 0.0; // -1 = crop out, 0 = no clamping
     extern bool clamp_final_coords = true; // false = crop out
 
     extern bool broken_freq = false;
     extern bool y_cos = false;
+    extern bool ref_other_axis = true;
 
     vec4 effect(vec4 color, Image tex, vec2 texture_coords, vec2 screen_coords) {
         vec2 chunk = vec2(
             thickness.x > 0.0 ? floor(texture_coords.x * texture_dim.x / thickness.x) * thickness.x : 0.0,
             thickness.y > 0.0 ? floor(texture_coords.y * texture_dim.y / thickness.y) * thickness.y : 0.0
         );
+        vec2 chunk_ref = chunk;
+        if (ref_other_axis)
+            chunk_ref = vec2(
+                thickness.x > 0.0 ? floor(texture_coords.y * texture_dim.y / thickness.x) * thickness.x : 0.0,
+                thickness.y > 0.0 ? floor(texture_coords.x * texture_dim.x / thickness.y) * thickness.y : 0.0
+            );
 
         if (thickness.x > 0.0) {
-            number x_siner = broken_freq ? (sine + chunk.x * diff_freq) * freq : sine * freq + chunk.x * diff_freq;
+            number x_diff = chunk_ref.x;
+            number x_siner = broken_freq ? (sine + x_diff * diff_freq) * freq : sine * freq + x_diff * diff_freq;
             texture_coords.x += sin(x_siner) * mag / texture_dim.x;
         }
         if (thickness.y > 0.0) {
-            number y_siner = broken_freq ? (sine + chunk.y * diff_freq) * freq : sine * freq + chunk.y * diff_freq;
+            number y_diff = chunk_ref.y;
+            number y_siner = broken_freq ? (sine + y_diff * diff_freq) * freq : sine * freq + y_diff * diff_freq;
             if (y_cos)
                 texture_coords.y += cos(y_siner) * mag / texture_dim.y;
             else
@@ -61,10 +70,9 @@ function Mod:postInit()
     Game.world:addFX(ShaderFX(Mod.WAVY_LAKE, {
         sine = function() return Kristal.getTime() end,
         texture_dim = {SCREEN_WIDTH, SCREEN_HEIGHT},
-        freq = 15,
-        broken_freq = true,
-        mag = 10,
-        clamp_chunk_dim = 1,
-        thickness = {5, 5}
+        clamp_chunk_dim = 0,
+        freq = 10,
+        mag = 5,
+        thickness = {5, 0}
     }))
 end
